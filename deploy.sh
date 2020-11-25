77efdf234474516a374aff5dda735253f3237559
Josh Deeden <jdeeden@protonmail.com>
2020-11-24 21:35:25 -0800

    Deploy that shit.

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t deeden/multi-client:latest -t deeden/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t deeden/multi-server:latest -t deeden/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t deeden/multi-worker:latest -t deeden/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+
+docker push deeden/multi-client:latest
+docker push deeden/multi-server:latest
+docker push deeden/multi-worker:latest
+docker push deeden/multi-client:$GIT_SHA
+docker push deeden/multi-server:$GIT_SHA
+docker push deeden/multi-worker:$GIT_SHA
+
+kubectl apply -f k8s
+kubcetl set image deployments/server-deployment server=deeden/multi-server:$GIT_SHA
+kubcetl set image deployments/client-deployment server=deeden/multi-client:$GIT_SHA
+kubcetl set image deployments/worker-deployment server=deeden/multi-worker:$GIT_SHA
